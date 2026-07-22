8f67aecef17e562359c355cca4b4a5267ec0032a
alvesbeto <aldemberto@gmail.com>
2026-07-21 21:42:56 -0300

    chore: Atualização projeto

--- php8.sh
+++ php8.sh
@@ -20,7 +20,7 @@ sudo apt update -y
 sudo pkcon update -y
 sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean
 
-sudo apt install openssl mcrypt php8.4 php8.4-mcrypt php8.4-common php8.4-mysql php8.4-sqlite3 php8.4-dom php8.4-bcmath php8.4-xml php8.4-xmlrpc php8.4-curl php8.4-gd php8.4-imagick php8.4-cli php8.4-dev php8.4-imap php8.4-mbstring php8.4-opcache php8.4-soap php8.4-zip php8.4-intl php8.4-cgi php8.4-pgsql php8.4-ldap php8.4-fpm -y
+sudo apt install openssl mcrypt php8.5 php8.5-mcrypt php8.5-common php8.5-mysql php8.5-sqlite3 php8.5-dom php8.5-bcmath php8.5-xml php8.5-xmlrpc php8.5-curl php8.5-gd php8.5-imagick php8.5-cli php8.5-dev php8.5-imap php8.5-mbstring php8.5-soap php8.5-zip php8.5-intl php8.5-cgi php8.5-pgsql php8.5-ldap php8.5-fpm -y
 
 ## Parar e remover serviço apache instalado com PHP 8 #
 sudo /etc/init.d/apache2 stop
